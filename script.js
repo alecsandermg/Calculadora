8454c22a20957ce89b35b0810b50823901d6408d
Alecsander <alecsandermg@hotmail.com>
2025-09-03 22:54:15 -0300

    added the functions insert, backspace, calculate and clear

--- script.js
+++ script.js
@@ -3,11 +3,38 @@ const botao = botoes.querySelector(".Botao")
 const linha1= document.querySelector("#linha1")
 const linha2= document.querySelector("#linha2")
 
-botao.addEventListener("click", leitura)
+var display1 = document.getElementById("linha1")
 
-function leitura()
+function insert(n)
 {
-linha1.innerHTML+= 'lido'
-linha2.innerHTML+= 'lido'
+    var display1 = document.getElementById("row1")
+    number = display1.innerHTML
+    if (number.length < 26 ){
+    display1.innerHTML = number + n
+    }
+    
 }
-console.log(nomePessoa)
+
+function clean()
+{
+    document.getElementById("row1").innerHTML = ''
+    document.getElementById("row2").innerHTML = ''
+}
+
+function back(){
+    var typed = document.getElementById("row1").innerHTML
+    document.getElementById("row1").innerHTML = typed.substring(0, typed.length -1)
+}
+
+function calculate()
+{
+    var typed = document.getElementById("row1").innerHTML
+    if (typed){
+    var result = document.getElementById("row2")
+    result.innerHTML = eval(typed)
+    document.getElementById("row1").innerHTML = ''
+    }
+    else{
+    window.alert("Digite alguma coisa...")
+    }
+}
