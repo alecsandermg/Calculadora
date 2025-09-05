5d20738c2cb68d9309047a3c9b232b93f696943d
Alecsander <alecsandermg@hotmail.com>
2025-09-05 19:50:21 -0300

    changed some operations symbols to fix the function calculate()

--- script.js
+++ script.js
@@ -4,11 +4,27 @@ const linha1= document.querySelector("#linha1")
 const linha2= document.querySelector("#linha2")
 
 var display1 = document.getElementById("linha1")
-
+var equation = ''
+ 
 function insert(n)
 {
     var display1 = document.getElementById("row1")
     number = display1.innerHTML
+    equation += n 
+    if (n.includes('/100*')){
+        n = '%'
+    }
+    if (n.includes('**2')){
+        n = '^2'
+        calculate()
+        equation = document.getElementById("row2").innerHTML
+        document.getElementById("row1").innerHTML = ''
+        document.getElementById("row1").innerHTML = equation
+    }
+    if (n.includes('*')){
+        n = 'X'
+    }
+
     if (number.length < 26 ){
     display1.innerHTML = number + n
     }
@@ -19,6 +35,7 @@ function clean()
 {
     document.getElementById("row1").innerHTML = ''
     document.getElementById("row2").innerHTML = ''
+    equation = ''
 }
 
 function back(){
@@ -31,10 +48,12 @@ function calculate()
     var typed = document.getElementById("row1").innerHTML
     if (typed){
     var result = document.getElementById("row2")
-    result.innerHTML = eval(typed)
+    result.innerHTML = eval(equation)
     document.getElementById("row1").innerHTML = ''
+    equation = ''
     }
     else{
     window.alert("Digite alguma coisa...")
     }
+    return
 }
